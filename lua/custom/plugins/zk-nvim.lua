7c23305e5f5e5a8788a474127b367883ca9ec13e
Pótfi Szabolcs <szabolcs.potfi@4sys.hu>
2024-09-19 17:10:06 +0200

    zk-nvim added

---
+++ lua/custom/plugins/zk-nvim.lua
@@ -0,0 +1,26 @@
+-- https://github.com/zk-org/zk-nvim
+
+return {
+  "zk-org/zk-nvim",
+  keys = {
+    -- Create a new note after asking for its title.
+    { "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "New Note" },
+
+    -- Open notes.
+    { "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", desc = "Open Note" },
+
+    -- Open notes associated with the selected tags.
+    { "<leader>zt", "<Cmd>ZkTags<CR>", desc = "Search Notes by tags" },
+
+    -- Search for the notes matching a given query.
+    { "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", desc = "Search Notes by query" },
+
+    -- Search for the notes matching the current visual selection.
+    { "<leader>zf", ":'<,'>ZkMatch<CR>", mode = "v", desc = "Search Notes" },
+  },
+  config = function()
+    require("zk").setup({
+      picker = "telescope"
+    })
+  end
+}
