7c8ad76109c6843d60c79b17b897f2fd2a70492d
Pótfi Szabolcs <szabolcs.potfi@4sys.hu>
2025-04-04 12:43:54 +0200

    nvim keymap esc change

--- init.lua
+++ init.lua
@@ -164,8 +164,8 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- remap esc to lk
-vim.keymap.set('i', 'lk', '<Esc>', { noremap = true })
+-- remap esc to lkj
+vim.keymap.set('i', 'lkj', '<Esc>', { noremap = true })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
