d58eb483d5f2df8fd1b64e5c869da910d3d86597
Pótfi Szabolcs <szabolcs.potfi@4sys.hu>
2025-05-19 20:44:03 +0200

    added nvim-window

---
+++ lua/custom/plugins/nvim-window.lua
@@ -0,0 +1,9 @@
+-- https://github.com/yorickpeterse/nvim-window
+
+return {
+  'yorickpeterse/nvim-window',
+  keys = {
+    { '<leader>wj', "<cmd>lua require('nvim-window').pick()<cr>", desc = 'nvim-window: Jump to window' },
+  },
+  config = true,
+}
