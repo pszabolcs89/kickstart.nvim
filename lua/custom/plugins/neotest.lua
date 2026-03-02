41c34ecf8a5ff357410e10f84b1ac19ecd7c4f93
Pótfi Szabolcs <potfi.szabolcs@edudev.hu>
2026-03-02 19:55:37 +0100

    added neotest

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,58 @@
+-- https://github.com/nvim-neotest/neotest
+
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-neotest/neotest-python',
+  },
+  keys = {
+    {
+      '<leader>dtt',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = 'Run nearest test',
+    },
+    {
+      '<leader>dtf',
+      function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end,
+      desc = 'Run file',
+    },
+    {
+      '<leader>dts',
+      function()
+        require('neotest').summary.toggle()
+      end,
+      desc = 'Toggle summary',
+    },
+    {
+      '<leader>dto',
+      function()
+        require('neotest').output_panel.toggle()
+      end,
+      desc = 'Toggle output panel',
+    },
+  },
+  config = function()
+    require('neotest').setup {
+      adapters = {
+        require 'neotest-python' {
+          dap = { justMyCode = false },
+          runner = 'pytest',
+          -- python = function()
+          --   -- prefer virtualenv python if present
+          --   local venv = vim.fn.getcwd() .. "/.venv/bin/python"
+          --   if vim.fn.executable(venv) == 1 then return venv end
+          --   return "python"
+          -- end,
+        },
+      },
+    }
+  end,
+}
